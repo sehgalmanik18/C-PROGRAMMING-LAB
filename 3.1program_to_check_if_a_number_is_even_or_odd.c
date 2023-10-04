58672dfc07ef6d0731bd64b603749be01f005f35
manik sehgal <146652574+sehgalmanik18@users.noreply.github.com>
2023-10-04 21:59:34 +0530

    Add files via upload

---
+++ 3.1program_to_check_if_a_number_is_even_or_odd.c
@@ -0,0 +1,15 @@
+#include<sTdio.h>
+void main()
+{
+    int a;
+    printf("enter any number\n");
+    scanf("%d",&a);
+    if(a % 2 == 0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+    }
